a4bc2890ae267a31bdd1d324c4c17cefbbaf019f
AlexeyStadnik <stadnik911@gmail.com>
2018-02-04 21:17:09 +0300

    Add swagger comment

--- src/main/java/com/stadnik/template/config/SwaggerConfig.java
+++ src/main/java/com/stadnik/template/config/SwaggerConfig.java
@@ -3,6 +3,9 @@ package com.stadnik.template.config;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+/**
+ * Enables Spring support of swagger
+ */
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
